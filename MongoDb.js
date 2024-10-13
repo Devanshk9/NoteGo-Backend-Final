3341eab2d51e0592028c4cd136f602fc93c38773
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2024-10-13 10:25:07 +0530

    Update MongoDb.js

--- MongoDb.js
+++ MongoDb.js
@@ -2,7 +2,9 @@ const mongodb=require('mongodb')
 
 // const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
 // const connectionUrl = "mongodb://localhost:27017/NoteGo"
-const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
+// const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
+const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true";
+
 
 
 const client=new mongodb.MongoClient(connectionUrl)
