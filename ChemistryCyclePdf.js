2c9a6b33f00a36ac3fc850c1a1378d4033e19f5e
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2024-10-14 00:04:13 +0530

    Update ChemistryCyclePdf.js

--- ChemistryCyclePdf.js
+++ ChemistryCyclePdf.js
@@ -16,8 +16,16 @@ router.post("/GetAllModules",async(req,resp)=>{
     if(MathsSem == "2CS" || MathsSem == "2EC" || MathsSem == "2ME")
         EnglishCourse="2"
 
-    var result = await PhysicsCycleCollection.find({Category:{ $in: [Cluster,"Common", "OneCredit",MathsSem,"ESC","ETC",EnglishCourse] }}).sort({SubjectNumber:1}).toArray()
-
+    var result
+    if(Cluster != "")
+        result = await PhysicsCycleCollection.find({Category:{ $in: [Cluster,"Common", "OneCredit",MathsSem,"ESC","ETC",EnglishCourse] }}).sort({SubjectNumber:1}).toArray()
+    else
+    if(EnglishCourse == 1)
+        result = await PhysicsCycleCollection.find({Category:{ $in: ["CS","EC","ME","Common", "OneCredit","1CS","1EC","1ME","ESC","ETC",EnglishCourse] }}).sort({SubjectNumber:1}).toArray()
+    else
+    if(EnglishCourse == 2)
+        result = await PhysicsCycleCollection.find({Category:{ $in: ["CS","EC","ME","Common", "OneCredit","2CS","2EC","2ME","ESC","ETC",EnglishCourse] }}).sort({SubjectNumber:1}).toArray()
+    
 
     console.log(result)
     resp.send(result)
