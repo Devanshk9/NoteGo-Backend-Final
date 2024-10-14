1b26bfb6f1505d2392cc8f9d1042378746590dac
Devansh Khetan <56186839+Devanshk9@users.noreply.github.com>
2024-10-14 20:52:32 +0530

    Update MongoDb.js

--- MongoDb.js
+++ MongoDb.js
@@ -1,27 +1,63 @@
-const mongodb=require('mongodb')
+// const mongodb=require('mongodb')
 
-// const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
-// const connectionUrl = "mongodb://localhost:27017/NoteGo"
-const connectionUrl = "mongodb+srv://notegobmsce:CUG6jysyvSXcVqZO@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
-// const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true";
+// // const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1";
+// // const connectionUrl = "mongodb://localhost:27017/NoteGo"
+// const connectionUrl = "mongodb+srv://notegobmsce:CUG6jysyvSXcVqZO@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
+// // const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&tlsAllowInvalidCertificates=true";
 
-// const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
+// // const connectionUrl = "mongodb+srv://notegobmsce:SpMVp1TnwezwT0HM@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
 
 
-const client=new mongodb.MongoClient(connectionUrl)
+// const client=new mongodb.MongoClient(connectionUrl)
 
-var db;
+// var db;
 
-try{
+// try{
     
-    client.connect();
-    console.log("Connected to Mongodb")
-    db=client.db()
+//     client.connect();
+//     console.log("Connected to Mongodb")
+//     db=client.db()
+// }
+
+// catch(err)
+// {
+//     console.log(err)
+// }
+
+// module.exports=db
+const mongodb = require('mongodb');
+
+// MongoDB connection URL
+const connectionUrl = "mongodb+srv://notegobmsce:CUG6jysyvSXcVqZO@cluster1.q0uhg.mongodb.net/NoteGo?retryWrites=true&w=majority&appName=Cluster1";
+
+// Connection options with connection pool settings
+const options = {
+  maxPoolSize: 10,           // Maximum number of connections in the pool (default is 100)
+  minPoolSize: 5,            // Minimum number of connections in the pool
+  serverSelectionTimeoutMS: 5000, // Timeout for selecting a server
+};
+
+// Create a new MongoClient with options
+const client = new mongodb.MongoClient(connectionUrl, options);
+
+let db;
+
+// Function to connect to MongoDB
+async function connectToMongoDB() {
+  try {
+    // Connect to the database
+    await client.connect();  // Await the connection
+    console.log("Connected to MongoDB");
+
+    // Assign the database instance to the 'db' variable
+    db = client.db('NoteGo');  // You can specify the DB name if needed, like client.db('NoteGo')
+  } catch (err) {
+    console.error("Error connecting to MongoDB:", err);
+  }
 }
 
-catch(err)
-{
-    console.log(err)
-}
+// Call the connection function immediately
+connectToMongoDB();
 
-module.exports=db
+// Export the database object for use in other parts of the app
+module.exports = db;
